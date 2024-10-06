5a6c1282305e66bdabaedfababd175c465e3f705
bossambani <bossambaka@gmail.com>
2024-10-06 09:26:35 +0300

    Implementatio of  a class named Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -31,4 +31,11 @@ export default class Pricing {
   displayFullPrice() {
     return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversion rate must be numbers');
+    }
+    return amount * conversionRate;
+  }
 }
