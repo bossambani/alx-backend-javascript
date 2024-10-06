e605440bb7e53e34c0ca5cf203d6647d1034c29e
bossambani <bossambaka@gmail.com>
2024-10-06 09:46:28 +0300

    Implementation of class Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('Square feet must be a number');
+    }
+    this._sqft = newSqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
