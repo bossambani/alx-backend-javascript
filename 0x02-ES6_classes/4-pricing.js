4f65be9c470f0e03e36c5e172ebee1fda19a72c1
bossambani <bossambaka@gmail.com>
2024-10-06 08:54:03 +0300

    Implementatio of  a class named Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (typeof newCurrency !== 'string') {
+      throw new TypeError('Currency must be a string');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}}`;
+  }
+}
