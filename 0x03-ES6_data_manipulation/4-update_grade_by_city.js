cc699c6fa841c16ab280b2e10718d4d96da4192a
bossambani <bossambaka@gmail.com>
2024-10-09 13:38:05 +0300

    function that returns an array of students for a specific city with their new grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,29 +1,29 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    //Check if students is an array
-    if (!Array.isArray(students)) {
-        return [];
-    }
+  // Check if students is an array
+  if (!Array.isArray(students)) {
+    return [];
+  }
 
-    //check if city is a string
-    if (typeof city !== 'string') {
-        return [];
-    }
+  // check if city is a string
+  if (typeof city !== 'string') {
+    return [];
+  }
 
-    //check if newGrades is an array
-    if (!Array.isArray(newGrades)) {
-        return [];
-    }
+  // check if newGrades is an array
+  if (!Array.isArray(newGrades)) {
+    return [];
+  }
 
-    return students.map((student) => {
-        if (student.location === city) {
-            const gradeRecord = newGrades.find((grade) => grade.studentId === student.id);
-            return {
-                ...student,
-                grade: gradeRecord ? gradeRecord.grade : 'N/A'
-            };
-        }
+  return students
+    .filter((student) => student !== undefined)
+    .map((student) => {
+      if (student.location === city) {
+        const gradeRecord = newGrades.find((grade) => grade.studentId === student.id);
+        return {
+          ...student,
+          grade: gradeRecord ? gradeRecord.grade : 'N/A',
+        };
+      }
+      return student;
     });
-
-
 }
-
