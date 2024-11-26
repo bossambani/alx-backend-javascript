395a1442e375bca326a0604a31d378be42e05ef5
bossambani <bossambaka@gmail.com>
2024-11-26 14:08:23 +0300

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,6 @@
-function displayMessage(Message) {
+//function that prints in STDOUT the string argument
+const displayMessage = (Message) => {
     console.log(Message);
-}
+};
 
 module.exports = displayMessage;
